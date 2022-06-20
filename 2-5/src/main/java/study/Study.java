2e19c062b44de61d695ead86c5d15ab262a7191b
inouetetsuji <81937796+inouetetsuji@users.noreply.github.com>
2022-06-20 13:38:31 +0900

    Add files via upload

--- 2-5/src/main/java/study/Study.java
+++ 2-5/src/main/java/study/Study.java
@@ -1,7 +1,10 @@
 package study;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -19,16 +22,15 @@ public class Study {
     public static final String SHOP_SHOHIN_01 = "牛乳";
     public static final String SHOP_SHOHIN_02 = "豚肉";
     public static final String SHOP_SHOHIN_03 = "コロッケ";
-	private static final Entry<String, Integer> entry = null;
 
     public static void main(String args[]) {
 
         // ① 定数を全て使って、String型のListを記述してください。
-        HashMap<Integer,String>shohinList = new HashMap<Integer,String>();
-		shohinList.put(0, "バナナ");
-    	shohinList.put(1, "牛乳");
-        shohinList.put(2, "豚肉");
-        shohinList.put(3, "コロッケ");
+        List<String>shohinList = new ArrayList<String>();
+		shohinList.add(SHOP_SHOHIN_00);
+    	shohinList.add(SHOP_SHOHIN_01);
+        shohinList.add(SHOP_SHOHIN_02);
+        shohinList.add(SHOP_SHOHIN_03);
 
      // ② 以下の「shopMap.put(shohinList.get(1), 180);」の処理について、コメントを記述してください。
         /*
@@ -44,22 +46,9 @@ public class Study {
 
         // ③ カリキュラムを参考に拡張for文を使って、課題の画像と同じ表示になるよう記述してください。
         // 「shohinList」と「shopMap」が保持する値を上手く利用しましょう。
-     for(String shopMap:shopMap) {
-    	 System.out.println(shopMap);
-     }
-
-
-
+     for(HashMap.Entry<String,Integer>entry:shopMap.entrySet()) {
+    	 System.out.println(entry.getKey()+"="+entry.getValue()+"円になります!");
     }
+ }
 
-	private static int extracted(int i) {
-		return i;
-	}
-
-	private static String getValue() {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
-	}
 }
-
-
